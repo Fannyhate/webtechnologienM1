3a9c1e6e5f1aeccfa8a39c111defa12ea1f02335
Fanny <s0552607@htw-berlin.de>
2024-02-09 12:52:59 -0800

    Add Grader properties files

---
+++ src/main/java/org/htwberlin/de/immo_mile_one/ImmoMileOneApplication.java
@@ -0,0 +1,13 @@
+package org.htwberlin.de.immo_mile_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoMileOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoMileOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/htwberlin/de/immo_mile_one/ImmoMileOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.htwberlin.de.immo_mile_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoMileOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
